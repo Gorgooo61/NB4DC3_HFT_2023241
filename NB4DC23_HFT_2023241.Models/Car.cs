90133af7ea7b3c6f32cd0e443d8c50a8ea0378ad
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-01 20:49:56 +0100

    car class extended

--- NB4DC23_HFT_2023241.Models/Car.cs
+++ NB4DC23_HFT_2023241.Models/Car.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NB4DC23_HFT_2023241.Models
 {
-    internal class Car
+    public class Car
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarID { get; set; }
+
+        [ForeignKey(nameof(Brand))]
+        public int BrandID { get; set; }
+
+        [StringLength(240)]
+        public string Model { get; set; }
+
+        public virtual Brand Brand { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string line)
+        {
+            string[] split = line.Split('#');
+            CarID = int.Parse(split[0]);
+            BrandID = int.Parse(split[1]);
+            Model = split[2];
+        }
     }
 }
