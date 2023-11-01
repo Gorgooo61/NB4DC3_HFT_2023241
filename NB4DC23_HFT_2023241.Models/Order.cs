7778d25bee4359bb7e51a6d523285b560d93e915
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-01 20:50:11 +0100

    order class extended

--- NB4DC23_HFT_2023241.Models/Order.cs
+++ NB4DC23_HFT_2023241.Models/Order.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NB4DC23_HFT_2023241.Models
 {
-    internal class Order
+    public class Order
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderID { get; set; }
+
+        [ForeignKey(nameof(Car))]
+        public int CarID { get; set; }
+
+        [Range(1, 7)]
+        public int HowManyDays { get; set; }
+        public virtual Car Car { get; set; }
+
+        public Order()
+        {
+
+        }
+        public Order(string line)
+        {
+            string[] split = line.Split('#');
+            OrderID = int.Parse(split[0]);
+            CarID = int.Parse(split[1]);
+            HowManyDays = int.Parse(split[2]);
+        }
     }
 }
