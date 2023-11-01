54e76313260be2649967a522e354d4fbc2b51298
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-01 16:09:24 +0100

    Brands class created

--- NB4DC23_HFT_2023241.Models/Brands.cs
+++ NB4DC23_HFT_2023241.Models/Brands.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NB4DC23_HFT_2023241.Models
 {
-    public class Brands
+    internal class Brands
     {
+        public string BrandID { get; set; }
+        public string BrandName { get; set; }
+        public string BrandCountry { get; set; }
     }
 }
