63862ecae317ec22ac51a970d45dd0e9916fd742
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-15 16:47:23 +0100

    Brand repo created

---
+++ NB4DC3_HFT_2023241.Repository/BrandRepository.cs
@@ -0,0 +1,29 @@
+﻿using NB4DC23_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB4DC3_HFT_2023241.Repository
+{
+    public class BrandRepository : Repository<Brand>, IRepository<Brand>
+    {
+        public BrandRepository(CarRentalDbContext ctx) :  base(ctx)
+        {
+            
+        }
+        public override Brand Read(int id)
+        {
+            return this.ctx.Brands.First(t => t.BrandID == id);
+        }
+        public override void Update(Brand item)
+        {
+            var old = Read(item.BrandID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+        }
+    }
+}
