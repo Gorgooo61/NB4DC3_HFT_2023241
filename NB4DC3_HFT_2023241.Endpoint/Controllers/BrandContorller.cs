40ce827146b0c7b603b01bd47c3f6bc41e0f66f5
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-29 20:24:47 +0100

    BrandController created and extended

---
+++ NB4DC3_HFT_2023241.Endpoint/Controllers/BrandContorller.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NB4DC23_HFT_2023241.Models;
+using NB4DC3_HFT_2023241.Logic;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NB4DC3_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        IBrandLogic logic;
+
+        public BrandController(IBrandLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<BrandContorller>
+        [HttpGet]
+        public IEnumerable<Brand> ReadAll()
+        {
+           return this.logic.ReadAll();
+        }
+
+        // GET api/<BrandContorller>/5
+        [HttpGet("{id}")]
+        public Brand Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<BrandContorller>
+        [HttpPost]
+        public void Create([FromBody] Brand value)
+        {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<BrandContorller>/5
+        [HttpPut]
+        public void Update([FromBody] Brand value)
+        {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<BrandContorller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
