203fb1d9c169bcd8de4c352fa9e72734eefe627a
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-29 20:24:59 +0100

    CarController created and extended

---
+++ NB4DC3_HFT_2023241.Endpoint/Controllers/CarController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NB4DC23_HFT_2023241.Models;
+using NB4DC3_HFT_2023241.Logic;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NB4DC3_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        ICarLogic logic;
+
+        public CarController(ICarLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<CarController>
+        [HttpGet]
+        public IEnumerable<Car> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        // GET api/<CarController>/5
+        [HttpGet("{id}")]
+        public Car Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<CarController>
+        [HttpPost]
+        public void Create([FromBody] Car value)
+        {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<CarController>/5
+        [HttpPut]
+        public void Update([FromBody] Car value)
+        {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<CarController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
