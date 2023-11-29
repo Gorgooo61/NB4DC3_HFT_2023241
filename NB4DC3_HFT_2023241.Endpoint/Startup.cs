28e3c74a0c24d391839b2b276376e709419b095a
Gergő Magyari <gergo.magyari5@gmail.com>
2023-11-29 20:25:28 +0100

    Endpoint modified

--- NB4DC3_HFT_2023241.Endpoint/Startup.cs
+++ NB4DC3_HFT_2023241.Endpoint/Startup.cs
@@ -3,10 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NB4DC23_HFT_2023241.Models;
+using NB4DC3_HFT_2023241.Logic;
+using NB4DC3_HFT_2023241.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace NB4DC3_HFT_2023241.Endpoint
 {
@@ -14,8 +21,28 @@ namespace NB4DC3_HFT_2023241.Endpoint
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<CarRentalDbContext>();
+
+            services.AddTransient<IRepository<Brand>, BrandRepository>();
+            services.AddTransient<IRepository<Car>, CarRepository>();
+            services.AddTransient<IRepository<Order>, OrderRepository>();
+
+            services.AddTransient<IBrandLogic, BrandLogic>();
+            services.AddTransient<ICarLogic, CarLogic>();
+            services.AddTransient<IOrderLogic, OrderLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "NB4DC3_HFT_2023241.Endpoint", Version = "v1" }); }) ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +51,27 @@ namespace NB4DC3_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NB4DC3_HFT_202324.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
